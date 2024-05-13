f16bd524ca19a5fb0364fa716a39cfe20aacb0fe
LuckyKumar926 <kumarlaxmangc@gmail.com>
2024-05-12 21:29:10 -0400

    code

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "auto-expire" {
   name          = "terraformtest-bucket"
-  project       = "test-cicd"
+  project       = "test-cicd-421815"
   location      = "US"
   force_destroy = true
 
