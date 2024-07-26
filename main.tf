0d8bb3a0419ff1512d5f9b0658eb7ecee204e924
LuckyKumar926 <kumarlaxmangc@gmail.com>
2024-07-26 12:11:05 -0400

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_bucket" "my-buckette" {
-  name          = "terraformtest-bucket"
-  project       = "test-cicd-421815"
-  location      = "US"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
-}
